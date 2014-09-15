08b48c33a0dbb61d3126be9ddd87893b4e1aaa64
BorisVarbanov <b.m.varbanov@gmail.com>
2014-09-15 20:58:51 +0300

    Adding Comments
    I have added a basic explanation to the program classes

--- src/breakout/Commons.java
+++ src/breakout/Commons.java
@@ -1,9 +1,9 @@
 package breakout;
 
-public interface Commons {
-    public static final int WIDTH = 300;
-    public static final int HEIGTH = 400;
-    public static final int BOTTOM = 390;
-    public static final int PADDLE_RIGHT = 250;
-    public static final int BALL_RIGHT = 280;
+public interface Commons { //Declares Game Constants
+    public static final int WIDTH = 300; //width of the screen
+    public static final int HEIGTH = 400; //height of the screen
+    public static final int BOTTOM = 390; //the end of the playing screen 10 pixels away from height
+    public static final int PADDLE_RIGHT = 250; //max distance the paddle can move on the screen(the paddle is 40 pixels wide)
+    public static final int BALL_RIGHT = 280; //max distance the ball can move on the screen(the ball is 10 pixels wide)
 }
