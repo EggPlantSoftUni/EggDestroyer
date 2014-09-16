c74362fc5b29567d3cedd98fe7596db0d7cd32de
Victoria <victoria.valkova@gmail.com>
2014-09-16 23:57:23 +0300

    Last merge
    from Svetlin

--- src/breakout/Brick.java
+++ src/breakout/Brick.java
@@ -6,53 +6,47 @@ import javax.swing.ImageIcon;
 public class Brick extends Sprite { //the Brick Class
 
     String brickie = "../images/brickie.png"; //creates the path of the used image
+    String crackedbrickie = "../images/brickie_cracked.png";
 
-    boolean destroyed; //bool that indicates whether a brick has been destroyed or not
-    
-    String brBrickie = "../images/brikieGreen.png";
-    
-    boolean cracked;  // bool that indicates whether an egg is cracked or not 
+    boolean destroyed;//bool that indicates whether a brick has been destroyed or not
+    boolean cracked;
 
-    
-    
+
     public Brick(int x, int y) { //decleration of a brick's x and y
       this.x = x;
       this.y = y;
+
+      ImageIcon ii = new ImageIcon(this.getClass().getResource(brickie)); //creates the brick image icon
+      image = ii.getImage(); //calls the "getImage" from "Sprites.java"
       
-	      if (!destroyed && !cracked) {	
-	      ImageIcon ii = new ImageIcon(this.getClass().getResource(brickie)); //creates the brick image icon
-	      image = ii.getImage(); //calls the "getImage" from "Sprites.java"
-	      }   
-	      else{
-	    	  ImageIcon ii = new ImageIcon(this.getClass().getResource(brBrickie)); //creates the brick image icon
-	          image = ii.getImage(); //calls the "getImage" from "Sprites.java"
-	      }
-	      
+      ImageIcon iii = new ImageIcon(this.getClass().getResource(crackedbrickie)); //creates the brick image icon
+      image1 = iii.getImage();
+
       width = image.getWidth(null); //set the width and height of the used image
       heigth = image.getHeight(null);
 
       destroyed = false; //declares that the created bicks have not been destroyed
-     // cracked = false;
+      cracked = false;
     }
-    
-        
-    
+
+
     public boolean isDestroyed() //function that checks whether a brick is destroyed or not
     {
-      return this.destroyed;
+      return destroyed;
     }
 
     public void setDestroyed(boolean destroyed) //changes the bool of a brick to destroyed == true when the ball hits a brick
     {
       this.destroyed = destroyed;
     }
-    
-    public boolean isCracked(){
-    	return this.cracked;
+    public boolean isCracked() //function that checks whether a brick is destroyed or not
+    {
+      return cracked;
     }
-    
-    public void setCracked(boolean cracked){
-    	this.cracked = cracked;
+
+    public void setCracked(boolean cracked) //changes the bool of a brick to destroyed == true when the ball hits a brick
+    {
+      this.cracked = cracked;
     }
     
 }
